2ffce8fa98b9197780ffab957080799df2f0a43c
Lauren Auchter <lsauchter@gmail.com>
2019-07-06 17:21:01 -0400

    Added a store file

--- src/App.js
+++ src/App.js
@@ -3,30 +3,12 @@ import './App.css';
 import Features from './Features.js';
 import Summary from './Summary.js';
 import Header from './Header.js';
+import STORE from './store.js';
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      selected: {
-        Processor: {
-            name: '17th Generation Intel Core HB (7 Core with donut spare)',
-            cost: 700
-          },
-        "Operating System": {
-            name: 'Ubuntu Linux 16.04',
-            cost: 200
-          },
-        "Video Card":{
-            name: 'Toyota Corolla 1.5v',
-            cost: 1150.98
-          },
-        Display: {
-            name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-            cost: 1500
-          }
-      }
-    }
+    this.state = { selected : STORE }
   }
 
   updateFeature(feature, newValue) {
