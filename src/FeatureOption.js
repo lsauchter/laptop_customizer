b582990dab597d1ca45e112a6d0d0e4cb2bf3cf2
Lauren Auchter <lsauchter@gmail.com>
2019-07-06 16:33:09 -0400

    Separated Feature logic and display

--- src/FeatureOption.js
+++ src/FeatureOption.js
@@ -3,32 +3,14 @@ import './FeatureOption.css';
 
 class FeatureOption extends Component {
     render() {
-        const features = Object.keys(this.props.features)
-          .map(key => {
-            const options = this.props.features[key].map((item, index) => {
-                console.log(item);
-              const selectedClass = item.name === this.props.selected[key].name ? 'feature__selected' : '';
-              const featureClass = 'feature__option ' + selectedClass;
-              return <li key={index} className="feature__item">
-                <div className={featureClass}
-                  onClick={e => this.props.handleUpdate(key, item)}>
-                    { item.name }
-                    ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                      .format(item.cost) })
-                </div>
-              </li>
-            });
-
-            return <div className="feature" key={key}>
-              <div className="feature__name">{key}</div>
-              <ul className="feature__list">
-                { options }
-              </ul>
-            </div>
-          });
         
         return (
-            <div>{features}</div>
+          <div className="feature" key={this.props.key}>
+          <div className="feature__name">{this.props.key}</div>
+          <ul className="feature__list">
+            { this.props.options }
+          </ul>
+        </div>
         )
     }
 }
--- src/Features.js
+++ src/Features.js
@@ -4,14 +4,29 @@ import FeatureOption from './FeatureOption.js';
 
 class Features extends Component {
     render() {  
+      const features = Object.keys(this.props.features)
+      .map(key => {
+        const options = this.props.features[key].map((item, index) => {
+            console.log(item);
+          const selectedClass = item.name === this.props.selected[key].name ? 'feature__selected' : '';
+          const featureClass = 'feature__option ' + selectedClass;
+          return <li key={index} className="feature__item">
+            <div className={featureClass}
+              onClick={e => this.props.handleUpdate(key, item)}>
+                { item.name }
+                ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
+                  .format(item.cost) })
+            </div>
+          </li>
+        });
+
+        return <FeatureOption key={key} options={options} />
+      });
           
           return (
             <section className="main__form">
             <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-            <FeatureOption
-                features={this.props.features}
-                selected={this.props.selected}
-                handleUpdate={this.props.handleUpdate}/>
+            {features}
             </section>
           )
     }
